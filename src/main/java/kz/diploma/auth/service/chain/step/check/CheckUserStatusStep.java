90c2ffbf81b87c6adcbcb5bf3084927e81122426
justdika <fragerrr73@gmail.com>
2024-06-02 19:56:05 +0500

    close session step

--- src/main/java/kz/diploma/auth/service/chain/step/check/CheckUserStatusStep.java
+++ src/main/java/kz/diploma/auth/service/chain/step/check/CheckUserStatusStep.java
@@ -1,7 +1,7 @@
 package kz.diploma.auth.service.chain.step.check;
 
 import kz.diploma.auth.service.chain.engine.AuthConditionalChainStep;
-import kz.diploma.auth.service.chain.step.GenerateApiKeyStep;
+import kz.diploma.auth.service.chain.step.CloseSessionStep;
 import kz.diploma.auth.service.chain.step.ex.AuthenticateExStep;
 import kz.diploma.auth.service.model.auth_params.InputAuthParams;
 import kz.diploma.auth.service.model.enums.Roles;
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CheckUserStatusStep extends AuthConditionalChainStep {
-    protected CheckUserStatusStep(GenerateApiKeyStep chainA, AuthenticateExStep chainB) {
+    protected CheckUserStatusStep(CloseSessionStep chainA, AuthenticateExStep chainB) {
         super(chainA, chainB);
     }
 
